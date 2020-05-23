f45e15dae58d705e06ecdcfdcc2b19fc1983368e
IDEHARA Norimichi <idehara@tama.ac.jp>
2020-05-23 03:13:34 +0000

    code seperation

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
+import days
+
 y,m = list(map(int, input().split(' ')))
 
-if m in (1,3,5,7,8,10,12):
-  d = 31
-elif m in (4,6,9,11):
-  d = 30
-else:
-  if y % 400 == 0:
-    d = 29
-  elif y % 100 == 0:
-    d = 28
-  elif y % 4 ==0: 
-    d = 29
-  else:
-    d = 28
+d = days.getDays(y,m)
 
 print(d)
